006326742b0cb97d2b425c90f5b667ee3e135ec7
Chris Lowe <clowe@luc.edu>
2014-07-09 21:55:26 -0500

    adds framework for JUnit/Gradle based unit tests

---
+++ javakiva/test/src/SomeClass.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertTrue;
+public class SomeClass { 
+
+@Test
+public void assertTrueIsTrue() {
+	assertTrue(true);
+}
+@Test
+public void assertTrueIsNotFalse() {
+	assertTrue(false);
+}
+
+
+}
